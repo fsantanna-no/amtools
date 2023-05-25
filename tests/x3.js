8c296fe506cfdb91a7d92ee0495c0aacf4624959
Francisco Sant'Anna <francisco.santanna@gmail.com>
2023-05-25 20:36:44 -0300

    ...

--- tests/x3.js
+++ tests/x3.js
@@ -8,7 +8,7 @@ const A2 = Automerge.change(A1, json => {
 })
 const chgA1 = Automerge.getChanges(A1,A2)
 const A3 = Automerge.change(A2, json => {
-    json.list.push('a1')
+    json.list = ['a1']
 })
 const chgA2 = Automerge.getChanges(A2,A3)
 
@@ -17,10 +17,10 @@ const chgA2 = Automerge.getChanges(A2,A3)
 const B1 = Automerge.init()
 const [B2] = Automerge.applyChanges(B1, chgA1)
 console.log(Automerge.getConflicts(B2))
-console.log(Automerge.getConflicts(B2,'list'))
 const B3 = Automerge.change(B2, json => {
-    json.list.push('b1')
+    json.list = ['b1']
 })
+console.log(Automerge.getConflicts(B3))
 const chgB1 = Automerge.getChanges(B2,B3)
 
 // SYNCHRONZIE A <-> B
@@ -30,7 +30,5 @@ const [B4] = Automerge.applyChanges(B3, chgA2)
 
 console.log(A4)
 console.log(B4)
-console.log(Automerge.getConflicts(B4))
 console.log(Automerge.getConflicts(B4,'list'))
-console.log(Automerge.getConflicts(A4))
 console.log(Automerge.getConflicts(A4,'list'))
